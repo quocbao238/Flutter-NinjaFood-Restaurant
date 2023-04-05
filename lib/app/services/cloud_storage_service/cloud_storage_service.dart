77924420b14905f5c71e72e662a1ed90656da063
Quoc bao <baobondev@gmail.com>
2023-04-06 00:25:46 +0700

    Create cloud_storage_service.dart

---
+++ lib/app/services/cloud_storage_service/cloud_storage_service.dart
@@ -0,0 +1,56 @@
+
+
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:ninjafood/app/helper/utils.dart';
+import 'package:ninjafood/app/services/services.dart';
+
+import 'cloud_storage_impl.dart';
+
+class CloudStorageService extends GetxService implements ServiceImpl, CloudStorageServiceImpl {
+  late final FirebaseStorage storage;
+
+
+
+  Future<void> call() async {
+    storage = FirebaseStorage.instance;
+  }
+
+  Future<String?> uploadAvatarImage({required File file, required String uid}) async {
+    try {
+      // Create a reference to the image location in Firebase Storage
+      final fileName = uid;
+      final ref = storage.ref().child('user/$fileName');
+      final metadata = SettableMetadata(
+        contentType: 'image/jpeg',
+        customMetadata: {'picked-file-path': file.path},
+      );
+      final uploadTask = await ref.putFile(File(file.path), metadata);
+      return await uploadTask.ref.getDownloadURL();
+    } catch (e) {
+      print('Error uploading image to Firebase Storage: $e');
+      return null;
+    }
+  }
+
+  @override
+  Future<String?> uploadImage({required File file}) async {
+    try {
+      // Create a reference to the image location in Firebase Storage
+      final fileName = createTimeStamp();
+      final ref = storage.ref().child('chatImages/$fileName');
+      final metadata = SettableMetadata(
+        contentType: 'image/jpeg',
+        customMetadata: {'picked-file-path': file.path},
+      );
+      final uploadTask = await ref.putFile(File(file.path), metadata);
+      return await uploadTask.ref.getDownloadURL();
+    } catch (e) {
+      print('Error uploading image to Firebase Storage: $e');
+      return null;
+    }
+  }
+
+}
