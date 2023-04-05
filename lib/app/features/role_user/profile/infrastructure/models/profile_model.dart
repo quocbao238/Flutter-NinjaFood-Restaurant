6ed0009294c5db9202515ff479989247b7d7ed9a
Ender <njckname.0co@gmail.com>
2023-04-05 09:16:32 +0700

    ProfileUI

---
+++ lib/app/features/role_user/profile/infrastructure/models/profile_model.dart
@@ -0,0 +1,44 @@
+class FavoriteFood {
+  String image;
+  String name;
+  int price;
+
+  FavoriteFood({
+    required this.image,
+    required this.name,
+    required this.price,
+  });
+
+  static List<FavoriteFood> favoriteItem = [
+    FavoriteFood(
+      image: 'assets/icons/food1.png',
+      name: 'Spacy fresh crab',
+      price: 35,
+    ),
+    FavoriteFood(
+      image: 'assets/icons/food2.png',
+      name: 'Spacy fresh crab',
+      price: 35,
+    ),
+    FavoriteFood(
+      image: 'assets/icons/food3.png',
+      name: 'Spacy fresh crab',
+      price: 35,
+    ),
+    FavoriteFood(
+      image: 'assets/icons/food3.png',
+      name: 'Spacy fresh crab',
+      price: 35,
+    ),
+    FavoriteFood(
+      image: 'assets/icons/food3.png',
+      name: 'Spacy fresh crab',
+      price: 35,
+    ),
+    FavoriteFood(
+      image: 'assets/icons/food3.png',
+      name: 'Spacy fresh crab',
+      price: 35,
+    ),
+  ];
+}
