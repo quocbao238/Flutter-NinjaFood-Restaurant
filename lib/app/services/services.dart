f6c26474f756dedd5f0914416fb1250f9a87b3a0
Quoc bao <baobondev@gmail.com>
2023-04-05 23:53:19 +0700

    Create services.dart

---
+++ lib/app/services/services.dart
@@ -0,0 +1,15 @@
+library services;
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:get/get.dart';
+import 'package:ninjafood/app/constants/contains.dart';
+import 'package:ninjafood/app/models/user_model.dart';
+
+import 'boot_services.dart';
+
+part 'auth_service.dart';
+// part 'firestore_service.dart';
+// part 'storage_service.dart';
+
