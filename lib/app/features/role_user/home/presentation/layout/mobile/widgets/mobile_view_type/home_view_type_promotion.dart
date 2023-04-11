19f8381a5552e50c48730b508d494c1ab6d6d3e2
Quoc bao <baobondev@gmail.com>
2023-04-11 13:55:45 +0700

    + PromotionItem

---
+++ lib/app/features/role_user/home/presentation/layout/mobile/widgets/mobile_view_type/home_view_type_promotion.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:ninjafood/app/features/role_user/home/presentation/layout/mobile/widgets/promotion_view/promotion_list.dart';
+
+class HomeViewTypePromotions extends StatelessWidget {
+  const HomeViewTypePromotions({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => PromotionListView();
+}
